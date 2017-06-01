e1f266fcb5c8449479099fd5d241c0caff311749
wuxianai <125701761@qq.com>
2017-06-01 20:06:58 +0800

    test001

--- app/src/main/java/com/example/dell/gittest/MainActivity.java
+++ app/src/main/java/com/example/dell/gittest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("Test","Test");
+        Log.e("Test","Test001");
     }
 }
